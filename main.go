f5d9ea12866a5c45683683ed491e778dbef45b94
Michael Mikhalevich <mike@viber.com>
2019-03-15 11:15:52 +0300

    Make function work for ascii characters set a-zA-Z

--- main.go
+++ main.go
@@ -6,29 +6,39 @@ import (
 	"os"
 )
 
-func sortChars(line []byte) []byte {
+// sorting characters in bytearray and returns a sorted copy with all letters mapped to their lower case
+// this function make deal with a-zA-z characters set, any other character will trigger error
+func sortCharsASCII(b []byte) ([]byte, error) {
 	chars := [26]int{}
-	for _, b := range line {
-		chars[b-'a']++
+	for _, c := range b {
+		if c >= 'A' && c <= 'Z' {
+			c += 'a' - 'A'
+		}
+
+		if c < 'a' || c > 'z' {
+			return nil, fmt.Errorf("Invalid character: %c in %s", c, string(b))
+		}
+		chars[c-'a']++
 	}
 
-	res := make([]byte, 0, len(line))
+	res := make([]byte, 0, len(b))
 	for i, n := range chars {
 		for j := 0; j < n; j++ {
 			res = append(res, byte(i)+'a')
 		}
 	}
-	return res
+	return res, nil
 }
 
-func findAnagrams(fileName string) error {
+// parse file and returns slice of agagrams
+func findAnagrams(fileName string) ([][]string, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer file.Close()
 
-	res := make(map[string][]string)
+	anagramCache := make(map[string][]string)
 
 	r := bufio.NewReader(file)
 	for {
@@ -36,24 +46,31 @@ func findAnagrams(fileName string) error {
 		if err != nil {
 			break
 		}
-		key := string(sortChars(l))
-		anagrams := res[key]
+
+		key, err := sortCharsASCII(l)
+		if err != nil {
+			return nil, err
+		}
+		anagrams := anagramCache[string(key)]
 		anagrams = append(anagrams, string(l))
-		res[key] = anagrams
+		anagramCache[string(key)] = anagrams
 	}
 
-	for _, v := range res {
+	var res [][]string
+	for _, v := range anagramCache {
 		if len(v) > 1 {
-			fmt.Println(v)
+			res = append(res, v)
 		}
 	}
 
-	return err
+	return res, err
 }
 
 func main() {
-	if err := findAnagrams("data.txt"); err != nil {
+	anagrams, err := findAnagrams("data.txt")
+	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(anagrams)
 	fmt.Println("Done...")
 }
